1ade662f2c557b56db46ac26f5f4abd2c97bfc97
Khaled Jallouli <6711@holbertonstudents.com>
2024-04-12 23:50:37 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import *
-from models import storage
+
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list():
+    """returns a list of all states in the database"""
+    from models import storage
+    from models.state import State
+
+    states = storage.all(State).values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """Remove the current SQLAlchemy session"""
+    from models import storage
+
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
